dec7aa18db01cf0b47fad11d99bd40cc8700f5dc
Katerina Tumakova <1katyakaterina@gmail.com>
2024-10-03 14:11:09 +0300

    Refactored App component

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import { useState } from 'react';
-import { Counter } from 'features';
+import { useState, useEffect } from 'react';
+import { Counter, Cards } from 'features';
+import { API_BASE_URL } from 'shared';
 
 /**
  * @typedef {import('./types').AppProps} AppProps
@@ -13,12 +14,28 @@ import { Counter } from 'features';
  */
 
 export const App = (props) => {
-  const [count, setCount] = useState(3);
+  const [count, setCount] = useState(9);
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const endPoint = `photos?_start=0&_limit=${count}`;
+        const response = await fetch(`${API_BASE_URL}/${endPoint}`);
+        if (!response.ok) throw new Error();
+        const cardsData = await response.json();
+        setCards(cardsData);
+      } catch (/** @type {*} */ error) {
+        console.error(error);
+      }
+    })();
+  }, [count]);
 
   return (
     <div className={'app'}>
       <h1>{props.title}</h1>
       <Counter count={count} setCount={setCount} />
+      <Cards cards={cards} />
     </div>
   );
 };
