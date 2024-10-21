7c38edf1eba78c790d21a9278e6e9f90814c581f
Katerina Tumakova <1katyakaterina@gmail.com>
2024-10-21 11:55:15 +0300

    Added Counter

---
+++ src/entity/Counter/Counter.js
@@ -0,0 +1,61 @@
+import classes from './Counter.module.scss';
+
+/**
+ * @typedef {import('./types').CounterProps} CounterProps
+ */
+
+/**
+ * @function Counter
+ * @param {CounterProps} props
+ * @returns {JSX.Element}
+ */
+
+export const Counter = (props) => {
+  const minCount = props.minCount ? props.minCount : 0;
+  const maxCount = props.maxCount ? props.maxCount : 20;
+  const isMinCount = props.count <= minCount;
+  const isMaxCount = props.count >= maxCount;
+
+  const handleDownClick = () => {
+    if (isMinCount) return;
+    props.setCount(props.count - 1);
+  };
+
+  const handleUpClick = () => {
+    if (isMaxCount) return;
+    props.setCount(props.count + 1);
+  };
+
+  const handleResetClick = () => {
+    props.setCount(minCount);
+  };
+
+  return (
+    <div className={classes.counter}>
+      {/* Title */}
+      <p className={classes.title}>
+        {props.name}: {props.count}
+      </p>
+      {/* Down */}
+      <button className={classes.button}
+        disabled={isMinCount}
+        onClick={handleDownClick}
+      >
+        Down
+      </button>
+      {/* Up */}
+      <button className={classes.button}
+        disabled={isMaxCount}
+        onClick={handleUpClick}
+      >
+        Up
+      </button>
+      {/* Reset */}
+      <button className={classes.button}
+        onClick={handleResetClick}
+      >
+        Reset
+      </button>
+    </div>
+  );
+};
---
+++ src/entity/Counter/types.ts
@@ -0,0 +1,7 @@
+export type CounterProps = {
+  name: string;
+  minCount: number;
+  count: number;
+  maxCount: number;
+  setCount: Function;
+};
