e21b09e620c8658ce0ab5f8378e3f14c32e946cd
Katerina Tumakova <1katyakaterina@gmail.com>
2024-10-21 20:26:01 +0300

    Added PhotosCounter

--- src/features/PhotosCounter/PhotosCounter.js
+++ src/features/PhotosCounter/PhotosCounter.js
@@ -1,64 +1,25 @@
-import classes from './PhotosCounter.module.scss';
 import { usePhotos } from 'shared/hooks';
+import { Counter } from 'entity';
 
 /**
- * @typedef {import('./types').CounterProps} CounterProps
+ * @typedef {import('./types').PhotosCounterProps} Props
  */
 
 /**
  * @function PhotosCounter
- * @param {CounterProps} props
+ * @param {Props} props
  * @returns {JSX.Element}
  */
 
 export const PhotosCounter = (props) => {
-  const photosStore = usePhotos();
-
-  const minCount = 0;
-  const maxCount = 20;
-  const isMinCount = props.count <= minCount;
-  const isMaxCount = props.count >= maxCount;
-
-  const handleDownClick = () => {
-    if (isMinCount) return;
-    props.setCount(props.count - 1);
-    photosStore.getPhotos(props.count - 1);
-  };
-
-  const handleUpClick = () => {
-    if (isMaxCount) return;
-    props.setCount(props.count + 1);
-    photosStore.getPhotos(props.count + 1);
-  };
-
-  const handleClearClick = () => {
-    if (isMaxCount) return;
-    props.setCount(0);
-    photosStore.resetPhotos();
-  };
+  const photosState = usePhotos();
 
   return (
-    <div className={classes.counter}>
-      <p className={classes.text}>
-        {props.name}: {props.count}
-      </p>
-      <button className={classes.button}
-        disabled={isMinCount}
-        onClick={handleDownClick}
-      >
-        Down
-      </button>
-      <button className={classes.button}
-        disabled={isMaxCount}
-        onClick={handleUpClick}
-      >
-        Up
-      </button>
-      <button className={classes.button}
-        onClick={handleClearClick}
-      >
-        Clear
-      </button>
-    </div>
+    <Counter name={props.name}
+      minCount={5}
+      count={photosState.photoCount}
+      maxCount={15}
+      setCount={photosState.setPhotoCount}
+    />
   );
 };
--- src/features/PhotosCounter/types.ts
+++ src/features/PhotosCounter/types.ts
@@ -1,5 +1,3 @@
-export type CounterProps = {
+export type PhotosCounterProps = {
   name: string;
-  count: number;
-  setCount: Function;
 };
