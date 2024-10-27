4a48251c7691754b4fd7f29def50d57251b0736c
Katerina Tumakova <1katyakaterina@gmail.com>
2024-10-27 22:58:37 +0300

    Refactored App and HomePage

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,9 @@
 import classes from './App.module.scss';
 import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { usePhotosStore, useTodosStore } from 'shared/hooks';
-import { HomePage, PhotoPage, TodoPage } from 'pages';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { usePhotosStore, useTodosStore, useUsersStore } from 'shared/hooks';
+import { HomePage, PhotoPage, PhotosPage, TodoPage, TodosPage, UserPage, UsersPage } from 'pages';
+import { Header } from 'widgets';
 
 /**
  * @typedef {import('./types').AppProps} AppProps
@@ -18,22 +19,26 @@ export const App = (props) => {
   const defaultCount = 5;
   const photosStore = usePhotosStore();
   const todosStore = useTodosStore();
+  const usersStore = useUsersStore();
 
   useEffect(() => {
     photosStore.setPhotoCount(defaultCount);
     todosStore.setTodoCount(defaultCount);
+    usersStore.setUserCount(defaultCount);
   }, []);
 
   return (
     <BrowserRouter>
       <div className={classes.app}>
-        <h1>
-          <Link to={'/'}>{props.title}</Link>
-        </h1>
+        <Header title={props.title}/>
         <Routes>
           <Route path={'/'} element={<HomePage />} />
+          <Route path={'/photos'} element={<PhotosPage />} />
+          <Route path={'/todos'} element={<TodosPage />} />
+          <Route path={'/users'} element={<UsersPage />} />
           <Route path={'/photo/:photoId/'} element={<PhotoPage />} />
           <Route path={'/todo/:todoId'} element={<TodoPage />} />
+          <Route path={'/user/:userId'} element={<UserPage />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import { Gallery, Tasks } from 'widgets';
+import classes from './HomePage.module.scss';
 
 /**
  * @function HomePage
@@ -6,10 +6,11 @@ import { Gallery, Tasks } from 'widgets';
  */
 
 export const HomePage = () => {
+  const defaultPhotoUrl = 'https://images.unsplash.com/photo-1607799279861-4dd421887fb3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NTgxNjh8MHwxfHNlYXJjaHwyfHxKYXZhU2NyaXB0fGVufDB8MHx8fDE3MzAwMjYzMTd8MA&ixlib=rb-4.0.3&q=80&w=1080';
+
   return (
-    <>
-      <Gallery />
-      <Tasks />
-    </>
+    <div className={classes.banner}>
+      <img src={defaultPhotoUrl} />
+    </div>
   );
 };
