5fe6ad772a9cbaa27210b2838cfb9f0ecbcf8422
Katerina Tumakova <1katyakaterina@gmail.com>
2024-11-12 00:25:39 +0300

    Refactored usePosts

--- src/shared/stores/posts/usePosts.js
+++ src/shared/stores/posts/usePosts.js
@@ -1,9 +1,12 @@
 import { create } from 'zustand';
-import { API_BASE_URL } from 'shared/config';
+import { API_FIREBASE_URL } from 'shared/config';
 import { partial } from 'shared/utils';
 
 /**
  * @typedef {import('./types').PostsStore} PostsStore
+ * @typedef {import('./types').PostForCreate} PostForCreate
+ * @typedef {import('./types').PostForUpdate} PostForUpdate
+ * @typedef {import('./types').PostForDelete} PostForDelete
  * @typedef {import('./types').StoreCreator} StoreCreator
  * @typedef {import('./types').SetterCallback} SetterCallback
  */
@@ -35,24 +38,30 @@ const getPosts = async (set, count) => {
       ...store,
       isPostsLoading: true,
       posts: [],
-      postsErrorMessage: '',
+      postsLoadErrorMessage: '',
     }));
-    const endPoint = `posts?_start=0&_limit=${count}`;
-    const response = await fetch(`${API_BASE_URL}/${endPoint}`);
+    const endPoint = `posts.json?orderBy="timestamp"&limitToLast=${count}`;
+    const response = await fetch(`${API_FIREBASE_URL}/${endPoint}`);
     if (!response.ok) throw new Error('posts not received');
-    const posts = await response.json();
+    const postsFromAPI = await response.json();
+    const posts = Object.entries(postsFromAPI)
+      .map(([id, postData]) => ({
+        id,
+        ...postData,
+      }))
+      .reverse();
     set(/** @type {SetterCallback} */ (store) => ({
       ...store,
       isPostsLoading: false,
       posts,
-      postsErrorMessage: '',
+      postsLoadErrorMessage: '',
     }));
   } catch (/** @type {*} */ error) {
     set(/** @type {SetterCallback} */ (store) => ({
       ...store,
       isPostsLoading: false,
       posts: [],
-      postsErrorMessage: error.message,
+      postsLoadErrorMessage: error.message,
     }));
   };
 };
@@ -71,36 +80,40 @@ const resetPosts = (set) => {
 };
 
 /**
- * @function getPostById
+ * @function getPost
  * @param {Function} set
  * @param {string} id
  * @returns {Promise<void>}
  */
 
-const getPostById = async (set, id) => {
+const getPost = async (set, id) => {
   try {
     set(/** @type {SetterCallback} */ (store) => ({
       ...store,
       isPostLoading: true,
       post: null,
-      postErrorMessage: '',
+      postLoadErrorMessage: '',
     }));
-    const endPoint = `posts/${id}`;
-    const response = await fetch(`${API_BASE_URL}/${endPoint}`);
+    const endPoint = `posts/${id}.json`;
+    const response = await fetch(`${API_FIREBASE_URL}/${endPoint}`);
     if (!response.ok) throw new Error('post not received');
-    const post = await response.json();
+    const postFromAPI = await response.json();
+    const post = {
+      ...postFromAPI,
+      id,
+    };
     set(/** @type {SetterCallback} */ (store) => ({
       ...store,
       isPostLoading: false,
       post,
-      postErrorMessage: '',
+      postLoadErrorMessage: '',
     }));
   } catch (/** @type {*} */ error) {
     set(/** @type {SetterCallback} */ (store) => ({
       ...store,
       isPostLoading: false,
       post: null,
-      postErrorMessage: error.message,
+      postLoadErrorMessage: error.message,
     }));
   };
 };
@@ -119,8 +132,179 @@ const resetPost = (set) => {
 };
 
 /**
+ * @function createPost
+ * @param {Function} set
+ * @param {PostForCreate} postForCreate
+ * @returns {Promise<void>}
+ */
+
+const createPost = async (set, postForCreate) => {
+  try {
+    set(/** @type {SetterCallback} */ (store) => ({
+      ...store,
+      isPostCreating: true,
+      isPostCreated: false,
+      postCreateErrorMessage: '',
+    }));
+    // const queryOpts = genQueryOpts('POST', postForCreate);
+    // const queryOpts = genQueryOpts('PUT', postForUpdate);
+    // const queryOpts = genQueryOpts('DELETE');
+    const queryOpts = {
+      method: 'POST',
+      body: JSON.stringify(postForCreate),
+      headers: { 'Content-type': 'application/json' },
+    };
+    const endPoint = `posts.json`;
+    const queryURL = `${API_FIREBASE_URL}/${endPoint}`;
+    const response = await fetch(queryURL, queryOpts);
+    if (!response.ok) throw new Error('Failed to create post');
+    const resData = await response.json();
+    set(/** @type {SetterCallback} */ (store) => ({
+      ...store,
+      isPostCreating: false,
+      isPostCreated: Boolean(resData),
+      postCreateErrorMessage: '',
+    }));
+  } catch (/** @type {*} */ error) {
+    const message = error.message;
+    set(/** @type {SetterCallback} */ (store) => ({
+      ...store,
+      isPostCreating: false,
+      isPostCreated: false,
+      postCreateErrorMessage: message,
+    }));
+    console.log(message);
+  };
+};
+
+/**
+ * @function resetPostCreation
+ * @param {Function} set
+ * @returns {void}
+ */
+
+const resetPostCreation = (set) => {
+  set(/** @type {SetterCallback} */ (store) => ({
+    ...store,
+    isPostCreating: false,
+    isPostCreated: false,
+    postCreateErrorMessage: '',
+  }));
+};
+
+/**
+ * @function updatePost
+ * @param {Function} set
+ * @param {PostForUpdate} postForUpdate
+ * @returns {Promise<void>}
+ */
+
+const updatePost = async (set, postForUpdate) => {
+  try {
+    set(/** @type {SetterCallback} */ (store) => ({
+      ...store,
+      isPostUpdating: true,
+      isPostUpdated: false,
+      postUpdateErrorMessage: '',
+    }));
+    const queryOpts = {
+      method: 'PUT',
+      body: JSON.stringify(postForUpdate),
+      headers: { 'Content-type': 'application/json' },
+    };
+    const endPoint = `posts/${postForUpdate.id}.json`;
+    const queryURL = `${API_FIREBASE_URL}/${endPoint}`;
+    const response = await fetch(queryURL, queryOpts);
+    if (!response.ok) throw new Error('Failed to update post');
+    const resData = await response.json();
+    set(/** @type {SetterCallback} */ (store) => ({
+      ...store,
+      isPostUpdating: false,
+      isPostUpdated: Boolean(resData),
+      postUpdateErrorMessage: '',
+    }));
+  } catch (/** @type {*} */ error) {
+    const message = error.message;
+    set(/** @type {SetterCallback} */ (store) => ({
+      ...store,
+      isPostUpdating: false,
+      isPostUpdated: false,
+      postUpdateErrorMessage: message,
+    }));
+  };
+};
+
+/**
+ * @function resetPostUpdate
+ * @param {Function} set
+ * @returns {void}
+ */
+
+const resetPostUpdate = (set) => {
+  set(/** @type {SetterCallback} */ (store) => ({
+    ...store,
+    isPostUpdating: false,
+    isPostUpdated: false,
+    postUpdateErrorMessage: '',
+  }));
+};
+
+/**
+ * @function deletePost
+ * @param {Function} set
+ * @param {string} postId
+ * @returns {Promise<void>}
+ */
+
+const deletePost = async (set, postId) => {
+  try {
+    set(/** @type {SetterCallback} */ (store) => ({
+      ...store,
+      isPostDeleting: true,
+      isPostDeleted: false,
+      postDeleteErrorMessage: '',
+    }));
+    const queryOpts = { method: 'DELETE' };
+    const endPoint = `posts/${postId}.json`;
+    const queryURL = `${API_FIREBASE_URL}/${endPoint}`;
+    const response = await fetch(queryURL, queryOpts);
+    if (!response.ok) throw new Error('Failed to delete post');
+    set(/** @type {SetterCallback} */ (store) => ({
+      ...store,
+      isPostDeleting: false,
+      isPostDeleted: response.ok,
+      postDeleteErrorMessage: '',
+    }));
+  } catch (/** @type {*} */ error) {
+    const message = error.message;
+    set(/** @type {SetterCallback} */ (store) => ({
+      ...store,
+      isPostDeleting: false,
+      isPostDeleted: false,
+      postDeleteErrorMessage: message,
+    }));
+    console.log(message);
+  };
+};
+
+/**
+ * @function resetPostDeletion
+ * @param {Function} set
+ * @returns {void}
+ */
+
+const resetPostDeletion = (set) => {
+  set(/** @type {SetterCallback} */ (store) => ({
+    ...store,
+    isPostDeleting: false,
+    isPostDeleted: false,
+    postDeleteErrorMessage: '',
+  }));
+};
+
+/**
  * @function usePosts
- * @returns {postsStore} postsStore
+ * @returns {PostsStore} PostsStore
  */
 
 export const usePosts = create(/** @type {StoreCreator} */ (set) => ({
@@ -128,17 +312,38 @@ export const usePosts = create(/** @type {StoreCreator} */ (set) => ({
   postCount: 0,
   setPostCount: partial(setPostCount, set),
 
-  /* state for posts store */
+  /* state for getting posts */
   isPostsLoading: false,
   posts: [],
-  postsErrorMessage: '',
+  postsLoadErrorMessage: '',
   getPosts: partial(getPosts, set),
   resetPosts: partial(resetPosts, set),
 
-  /* state for post store */
+  /* state for getting post */
   isPostLoading: false,
   post: null,
-  postErrorMessage: '',
-  getPostById: partial(getPostById, set),
+  postLoadErrorMessage: '',
+  getPost: partial(getPost, set),
   resetPost: partial(resetPost, set),
+
+  /* state for create post */
+  isPostCreating: false,
+  isPostCreated: false,
+  postCreateErrorMessage: '',
+  createPost: partial(createPost, set),
+  resetPostCreation: partial(resetPostCreation, set),
+
+  /* state for update post */
+  isPostUpdating: false,
+  isPostUpdated: false,
+  postUpdateErrorMessage: '',
+  updatePost: partial(updatePost, set),
+  resetPostUpdate: partial(resetPostUpdate, set),
+
+  /* state for delete post */
+  isPostDeleting: false,
+  isPostDeleted: false,
+  postDeleteErrorMessage: '',
+  deletePost: partial(deletePost, set),
+  resetPostDeletion: partial(resetPostDeletion, set),
 }));
