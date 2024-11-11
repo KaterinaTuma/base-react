4ea42f0c1a6c91e95e751a2266d7c590a59b18d8
Katerina Tumakova <1katyakaterina@gmail.com>
2024-11-12 00:21:41 +0300

    Added icons

---
+++ src/shared/icons/index.js
@@ -0,0 +1,6 @@
+// @ts-nocheck
+export { ReactComponent as IconError } from './svg/error.svg';
+export { ReactComponent as IconSuccess } from './svg/success.svg';
+export { ReactComponent as IconClose } from './svg/close.svg';
+export { ReactComponent as IconEdit } from './svg/edit.svg';
+export { ReactComponent as IconDelete } from './svg/delete.svg';
