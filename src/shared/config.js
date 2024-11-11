6b57052c4de8b59765ee71c5e81e8d5fe75919fd
Katerina Tumakova <1katyakaterina@gmail.com>
2024-11-12 00:25:15 +0300

    Added config

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,1 +1,3 @@
 export const API_BASE_URL = 'https://jsonplaceholder.typicode.com';
+export const API_FIREBASE_URL = 'https://base-react-dd558-default-rtdb.europe-west1.firebasedatabase.app';
+export const DEFAULT_USER_ID = '001';
