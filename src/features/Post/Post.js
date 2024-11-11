0a0daa7572712963871345f2993128d4d34cd7b3
Katerina Tumakova <1katyakaterina@gmail.com>
2024-11-12 00:20:05 +0300

    Refactored Post

---
+++ src/features/Post/Post.js
@@ -0,0 +1,11 @@
+import { Counter } from './Counter';
+import { Creator } from './Creator';
+import { Editor } from './Editor';
+import { Deleter } from './Deleter';
+
+export const Post = {
+  Counter,
+  Creator,
+  Editor,
+  Deleter,
+};
